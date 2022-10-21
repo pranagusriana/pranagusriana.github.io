573f6855049923c267bca9295217c5ab5498d47a
pranagusriana <pranagusriana@gmail.com>
2022-10-21 18:36:46 +0700

    Add send me a message feature

---
+++ assets/js/contactme.js
@@ -0,0 +1,20 @@
+function formSubmit(event) {
+    event.preventDefault();
+    var url = "https://script.google.com/macros/s/AKfycbzPFMSI512-INdf6RBw3zILC4yt1f49a7NoBYsZQt0GnO1McbI-ntgGVkyyjqS3dKo/exec";
+    var request = new XMLHttpRequest();
+    request.open('POST', url, true);
+    request.onload = function() { 
+        // request successful
+        // we can use server response to our request now
+        alert(request.responseText);
+        event.target.reset();
+    };
+
+    request.onerror = function() {
+        alert(request.responseText)
+    };
+
+    request.send(new FormData(event.target)); // create FormData from form that triggered event
+}
+
+document.getElementById("contact-form").addEventListener("submit", formSubmit);
